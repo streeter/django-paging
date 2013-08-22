89edbc18d06a835ce76d1dc23c651785f7af352d
Chris Streeter <streeter@users.noreply.github.com>
2013-08-22 08:57:44 -0700

    Correctly retrieve the right package to get version

--- paging/__init__.py
+++ paging/__init__.py
@@ -1,5 +1,5 @@
 try:
     VERSION = __import__('pkg_resources') \
-        .get_distribution('django-sentry').version
+        .get_distribution('django-paging').version
 except Exception, e:
     VERSION = 'unknown'
